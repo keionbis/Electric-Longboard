f6047091247dc81cf355953b482ffd06d9c5828f
keionbis <keion96@gmail.com>
2017-12-03 02:08:00 -0500

    Added Board files and start to firmware code, this is a modified version of the esp-now code, much more still to be added

---
+++ BMS Firmware/BMS Firmware.ino
@@ -0,0 +1,68 @@
+#include <esp_now.h>
+#include <WiFi.h>
+#include "esp32-hal-ledc.h"
+#define CHANNEL 1
+#define COUNT_LOW 0
+#define COUNT_HIGH 8888
+#define TIMER_WIDTH 16
+
+
+// Init ESP Now with fallback
+void InitESPNow() {
+  if (esp_now_init() == ESP_OK) {
+    Serial.println("ESPNow Init Success");
+  }
+  else {
+    Serial.println("ESPNow Init Failed");
+    // Retry InitESPNow, add a counte and then restart?
+    // InitESPNow();
+    // or Simply Restart
+    ESP.restart();
+  }
+}
+
+// config AP SSID
+void configDeviceAP() {
+  char* SSID = "Slave_1";
+  bool result = WiFi.softAP(SSID, "Slave_1_Password", CHANNEL, 0);
+  if (!result) {
+    Serial.println("AP Config failed.");
+  } else {
+    Serial.println("AP Config Success. Broadcasting with AP: " + String(SSID));
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  ledcSetup(1, 50, TIMER_WIDTH); // channel 1, 50 Hz, 16-bit width
+  ledcAttachPin(2, 1);   // GPIO 22 assigned to channel 1
+  Serial.println("ESPNow/Basic/Slave Example");
+  //Set device in AP mode to begin with
+  WiFi.mode(WIFI_AP);
+  // configure device AP mode
+  configDeviceAP();
+  // This is the mac address of the Slave in AP Mode
+  Serial.print("AP MAC: "); Serial.println(WiFi.softAPmacAddress());
+  // Init ESPNow with a fallback logic
+  InitESPNow();
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info.
+  esp_now_register_recv_cb(OnDataRecv);
+}
+
+// callback when data is recv from Master
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print("Last Packet Recv from: "); Serial.println(macStr);
+  Serial.print("Last Packet Recv Data: "); Serial.println(*data);
+  Serial.println("");
+  ledcWrite(1,map(*data,0,255,0,65535));
+  delay(10);
+  
+}
+
+void loop() {
+  // Chill
+}
